8b4990ae13ac3d35120c1e7b42f99bf8bfe42971
Данов Константин <boss.danov1337@bebra.ru>
2023-11-27 23:13:51 +0300

    Мелкие изменения вида

--- base/main.cpp
+++ base/main.cpp
@@ -5,7 +5,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QApplication::setStyle("Fusion");
     MainWindow w;
+    w.setWindowTitle("Алкогольный рай");
     w.show();
     return a.exec();
 }
