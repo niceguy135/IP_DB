c19287f7d419a4845098d32cbead445112af6d5c
Данов Константин <boss.danov1337@bebra.ru>
2023-11-29 02:03:20 +0300

    Добавил скелет программы

--- base/mainwindow.cpp
+++ base/mainwindow.cpp
@@ -6,10 +6,37 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    initDatabaseInterfaces();
+
+    // Stacked widget является скелетом приложения
+    stackedWidget = new QStackedWidget();
+
+    stackedWidget->addWidget(_login);
+    stackedWidget->addWidget(_drinkTypeWidget);
+    stackedWidget->addWidget(_orderswidget);
+    stackedWidget->addWidget(_rootWidget);
+
+    stackedWidget->setCurrentWidget(_login);
+
+    setCentralWidget(stackedWidget);
 }
+
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+
+void MainWindow::initDatabaseInterfaces()
+{
+    database_unit_ = std::make_unique<SqlService>();
+    database_unit_->databaseConnect();
+
+    // Setting for interfaces
+    _login = new login();
+    _drinkTypeWidget = new drinkTypeWidget();
+    _orderswidget = new orderswidget();
+    _rootWidget = new rootWidget();
+}
--- base/mainwindow.h
+++ base/mainwindow.h
@@ -1,7 +1,17 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <memory.h>
+
 #include <QMainWindow>
+#include <QStackedWidget>
+
+#include "authentification/login.h"
+#include "selecting/drinktypewidget.h"
+#include "selecting/orderswidget.h"
+#include "selecting/rootwidget.h"
+
+#include <databaseStuff/sqlservice.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +24,15 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+    login* _login;
+    drinkTypeWidget* _drinkTypeWidget;
+    orderswidget* _orderswidget;
+    rootWidget* _rootWidget;
+
+    QStackedWidget *stackedWidget;
+    std::shared_ptr<SqlService> database_unit_;
+    void initDatabaseInterfaces();
 
 private:
     Ui::MainWindow *ui;
